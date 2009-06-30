71c1106116e198d863c07a9ee39ab0658501fc7e
Alex Rabarts <alexrabarts@gmail.com>
2009-06-30 12:12:33 +0100

    Migrate to Shoulda for clearer tests

--- test/tld_test.rb
+++ test/tld_test.rb
@@ -1,52 +1,53 @@
 require 'test/unit'
 require 'tld'
+require 'shoulda'
 
 class TestTld < Test::Unit::TestCase
-  def test_find_tld_by_exact_match
+  should 'find TLD by exact match' do
     assert_equal 'au', TLD.find('au').tld
   end
 
-  def test_find_tld_by_exact_match_with_dot
+  should 'find TLD by exact match with dot' do
     assert_equal 'au', TLD.find('.au').tld
   end
 
-  def test_find_tld_with_case
+  should 'find TLD with case' do
     assert_equal 'au', TLD.find('AU').tld
   end
 
-  def test_find_tld_by_hostname
+  should 'find TLD by hostname' do
     assert_equal 'au', TLD.find('foo.bar.au').tld
   end
 
-  def test_find_tld_by_url
+  should 'find TLD by url' do
     assert_equal 'au', TLD.find('http://foo.bar.au/baz').tld
   end
 
-  def test_get_tld_currency
+  should 'get TLD currency' do
     assert_equal 'AUD', TLD.find('au').currency
   end
 
-  def test_get_mapped_tld_currency
+  should 'get mapped TLD currency' do
     assert_equal 'EUR', TLD.find('eu').currency
     assert_equal 'GBP', TLD.find('uk').currency
   end
 
-  def test_get_tld_name
+  should 'get TLD name' do
     assert_equal 'Australia', TLD.find('au').name
     assert_equal 'Business', TLD.find('biz').name
   end
 
-  def test_get_iso_alpha3_code
+  should 'get iso alpha3 code' do
     assert_equal 'AUS', TLD.find('au').alpha3
     assert_equal 'GBR', TLD.find('uk').alpha3
   end
 
-  def test_com_should_map_to_empty_currency_not_comoros
+  should 'com should map to empty currency (not Comoros)' do
     assert_equal [], TLD.find('com').currencies
     assert_nil TLD.find('com').currency
   end
 
-  def test_unknown_tld
+  should 'raise exception when TLD is unknown' do
     assert_raises TLD::UnknownTldError do
       TLD.find('foo')
     end
